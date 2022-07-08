268e0ac1328cf55b21f33842afe480519a0e019c
Danny Dowling <danny.dowling@gmail.com>
2022-07-08 08:48:18 -0800

    updates

--- Registry Change Display/Form1.cs
+++ Registry Change Display/Form1.cs
@@ -5,39 +5,54 @@ namespace Registry_Change_Display
 {
     public partial class Form1 : Form
     {
-        List<string> changes = new List<string>();
-
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void create_Initial_Snapshot_Click(object sender, EventArgs e)
         {
             string path = Path.GetDirectoryName(Application.ExecutablePath);
-            try
-            {
+          
                 string command1 = string.Format(@"dir -rec -erroraction ignore HKLM:\ | %name > {0}\Base - HKLM.txt", path);
                 StringBuilder builder = new StringBuilder();
                 builder.Append(command1);
                 run_PowerShell_Command(builder.ToString());
-            }
-            catch (Exception)
-            {
-
-            }
-            finally
-            {
+         
                 string command2 = string.Format(@"dir -rec -erroraction ignore HKCU:\ | % name > {0}\Base - HKCU.txt", path);
                 StringBuilder builder = new StringBuilder();
                 builder.Append(command2);
+                run_PowerShell_Command(builder.ToString());            
+        }
+
+        private void List_Changes_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetDirectoryName(Application.ExecutablePath);
+
+            StringBuilder builder = new StringBuilder();
+           
+                string current_registry_HKCU_command = string.Format(
+                    $@"dir -rec -erroraction ignore HKCU:\ | % name > {0}/Current-HKCU-{1}).txt", path, DateTime.Now.ToShortDateString());
+                builder.Clear();
+                builder.Append(current_registry_HKCU_command);
                 run_PowerShell_Command(builder.ToString());
-            }
+
+                string current_registry_HKLM_command = string.Format(
+                    $@"dir -rec -erroraction ignore HKLM:\ | % name > {0}/Current-HKLM-{1}).txt", path, DateTime.Now.ToShortDateString());
+                builder.Clear();
+                builder.Append(current_registry_HKLM_command);
+                run_PowerShell_Command(builder.ToString());
+          
+                string compare_HKCU_registry_changes_command = string.Format(
+                    $@"Compare-Object (Get-Content -Path .\Base-HKCU.txt)(Get-Content-Path .\{0}/Current-HKCU-{1}).txt)", path, DateTime.Now.ToShortDateString());
+                builder.Clear();
+                builder.Append(compare_HKCU_registry_changes_command);
+                run_PowerShell_Command(builder.ToString());
+
+                string compare_HKLM_registry_changes_command = string.Format(
+                    $@"Compare-Object (Get-Content -Path .\Base-HKLM.txt)(Get-Content-Path .\{0}/Current-HKLM-{1}).txt)", path, DateTime.Now.ToShortDateString());
+                builder.Clear();
+                builder.Append(compare_HKLM_registry_changes_command);
+                run_PowerShell_Command(builder.ToString());
         }
 
         StreamReader standardOutput;
@@ -53,56 +68,23 @@ namespace Registry_Change_Display
 
             process.Start();
             process.StandardInput.WriteLine(command);
-            standardOutput = process.StandardOutput;
+
+            process.OutputDataReceived += (sender, args) => Display(args.Data);
+            process.ErrorDataReceived += (sender, args) => Display(args.Data);
+
             process.CloseMainWindow();
             process.Dispose();
         }
 
-        private void List_Changes_Click(object sender, EventArgs e)
+        SynchronizationContext _syncContext;
+        void Display(string output)
         {
-            string path = Path.GetDirectoryName(Application.ExecutablePath);
+            _syncContext.Post(_ => listBox1.Items.Add(output), null);
+        }
 
-            StringBuilder builder = new StringBuilder();
-            try
-            {
-                string current_registry_HKCU_command = string.Format(
-                    $@"dir -rec -erroraction ignore HKCU:\ | % name > {0}/Current-HKCU-$(get-date -f yyyy-MM-dd).txt", path);
-                builder.Clear();
-                builder.Append(current_registry_HKCU_command);
-                run_PowerShell_Command(builder.ToString());
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
-                string current_registry_HKLM_command = string.Format(
-                    $@"dir -rec -erroraction ignore HKLM:\ | % name > {0}/Current-HKLM-$(get-date -f yyyy-MM-dd).txt", path);
-                builder.Clear();
-                builder.Append(current_registry_HKLM_command);
-                run_PowerShell_Command(builder.ToString());
-            }            
-            catch (Exception)
-            {
-
-            }
-            finally
-            {
-                string compare_HKCU_registry_changes_command = string.Format(
-                    $@"Compare-Object (Get-Content -Path .\Base-HKCU.txt)(Get-Content-Path .\{0}/Current-HKCU-$(get-date -f yyyy-MM-dd).txt)", path);
-                builder.Clear();
-                builder.Append(compare_HKCU_registry_changes_command);
-                run_PowerShell_Command(builder.ToString());
-
-                string compare_HKLM_registry_changes_command = string.Format(
-                    $@"Compare-Object (Get-Content -Path .\Base-HKLM.txt)(Get-Content-Path .\{0}/Current-HKLM-$(get-date -f yyyy-MM-dd).txt)", path);
-                builder.Clear();
-                builder.Append(compare_HKLM_registry_changes_command);
-                run_PowerShell_Command(builder.ToString());
-
-                using (standardOutput)
-                {
-                    foreach (var line in standardOutput.ReadToEnd().Split("/n"))
-                    {
-                        listBox1.Items.Add(line);
-                    }
-                }
-            }
         }
     }
 }
