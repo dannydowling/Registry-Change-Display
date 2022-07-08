3044c3427037dd97d14314d0a7065a376cba9430
Danny Dowling <danny.dowling@gmail.com>
2022-07-08 07:15:46 -0800

    updates

--- Registry Change Display/Form1.cs
+++ Registry Change Display/Form1.cs
@@ -62,22 +62,45 @@ namespace Registry_Change_Display
         {
             string path = Path.GetDirectoryName(Application.ExecutablePath);
 
-            string current_registry_command = string.Format(@"dir -rec -erroraction ignore HKCU:\ | % name > {0}\HKCU-{1}.txt", path, DateTime.Now.ToShortDateString());
             StringBuilder builder = new StringBuilder();
-            builder.Append(current_registry_command);
-            run_PowerShell_Command(builder.ToString());
-
-            string compare_registry_changes_command = string.Format(@"Compare -Object(Get-Content-Path.\Base-HKCU.txt)(Get-Content-Path.\{0}\HKCU-{1}.txt)", path, DateTime.Now.ToShortDateString());
-            builder.Clear();
-            builder.Append(compare_registry_changes_command);
-            run_PowerShell_Command(builder.ToString());
-           
-            
-            using (standardOutput)
+            try
             {
-                foreach (var line in standardOutput.ReadLine())
+                string current_registry_HKCU_command = string.Format(
+                    $@"dir -rec -erroraction ignore HKCU:\ | % name > {0}/Current-HKCU-$(get-date -f yyyy-MM-dd).txt", path);
+                builder.Clear();
+                builder.Append(current_registry_HKCU_command);
+                run_PowerShell_Command(builder.ToString());
+
+                string current_registry_HKLM_command = string.Format(
+                    $@"dir -rec -erroraction ignore HKLM:\ | % name > {0}/Current-HKLM-$(get-date -f yyyy-MM-dd).txt", path);
+                builder.Clear();
+                builder.Append(current_registry_HKLM_command);
+                run_PowerShell_Command(builder.ToString());
+            }            
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                string compare_HKCU_registry_changes_command = string.Format(
+                    $@"Compare-Object (Get-Content -Path .\Base-HKCU.txt)(Get-Content-Path .\{0}/Current-HKCU-$(get-date -f yyyy-MM-dd).txt)", path);
+                builder.Clear();
+                builder.Append(compare_HKCU_registry_changes_command);
+                run_PowerShell_Command(builder.ToString());
+
+                string compare_HKLM_registry_changes_command = string.Format(
+                    $@"Compare-Object (Get-Content -Path .\Base-HKLM.txt)(Get-Content-Path .\{0}/Current-HKLM-$(get-date -f yyyy-MM-dd).txt)", path);
+                builder.Clear();
+                builder.Append(compare_HKLM_registry_changes_command);
+                run_PowerShell_Command(builder.ToString());
+
+                using (standardOutput)
                 {
-                    listBox1.Items.Add(line);
+                    foreach (var line in standardOutput.ReadToEnd().Split("/n"))
+                    {
+                        listBox1.Items.Add(line);
+                    }
                 }
             }
         }
