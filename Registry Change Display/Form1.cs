381a4f6cd290d7982be73773c9f56149f6b9b49e
Danny Dowling <danny.dowling@gmail.com>
2022-07-11 19:35:09 -0800

    updates

--- Registry Change Display/Form1.cs
+++ Registry Change Display/Form1.cs
@@ -14,15 +14,15 @@ namespace Registry_Change_Display
             string path = Path.GetDirectoryName(Application.ExecutablePath);
 
             //create the files and then pipe to them the data for the base snapshot.
-            string HKCU_Init_FilePath = string.Format($@"{0}/Base-HKCU.txt", path);
+            string HKCU_Init_FilePath = string.Format($@"{0}\\Base-HKCU.txt", path);
             File.CreateText(HKCU_Init_FilePath);
             string command1 = string.Format($@"dir -rec -erroraction ignore HKCU:\ | %name > {0}", HKCU_Init_FilePath);
                 StringBuilder builder = new StringBuilder();
                 builder.Append(command1);
                 run_PowerShell_Command(builder.ToString());
 
-            string HKLM_Init_FilePath = string.Format($@"{0}/Base-HKLM.txt", path);
-            File.CreateText(HKLM_Init_FilePath);
+            string HKLM_Init_FilePath = string.Format($@"{0}\\Base-HKLM.txt", path);
+            File.Create(HKLM_Init_FilePath);
             string command2 = string.Format($@"dir -rec -erroraction ignore HKLM:\ | % name > {0}", HKLM_Init_FilePath);
                 StringBuilder builder2 = new StringBuilder();
                 builder.Append(command2);
@@ -34,8 +34,8 @@ namespace Registry_Change_Display
             string path = Path.GetDirectoryName(Application.ExecutablePath);
 
             //create the file for the current state of HKCU and push data into it.
-            string HKCU_Current_FilePath = string.Format($@"{0}/Current-HKCU-{1}).txt", path, DateTime.Now.ToShortDateString());
-            File.CreateText(HKCU_Current_FilePath);
+            string HKCU_Current_FilePath = string.Format($@"{0}\\Current-HKCU-{1}).txt", path, DateTime.Now.ToShortDateString());
+            File.Create(HKCU_Current_FilePath);
                 StringBuilder Current_HKCU_stringbuilder = new StringBuilder();           
                 string current_registry_HKCU_command = string.Format(
                     $@"dir -rec -erroraction ignore HKCU:\ | % name > {0}", HKCU_Current_FilePath);
@@ -46,7 +46,7 @@ namespace Registry_Change_Display
           //then do the actual comparison
             StringBuilder Compare_HKCU_stringbuilder = new StringBuilder();
             string compare_HKCU_registry_changes_command = string.Format(
-                    $@"Compare-Object (Get-Content -Path .\Base-HKCU.txt)(Get-Content-Path {0}", HKCU_Current_FilePath);
+                    $@"Compare-Object (Get-Content -Path .\\Base-HKCU.txt)(Get-Content-Path {0}", HKCU_Current_FilePath);
             Compare_HKCU_stringbuilder.Clear();
             Compare_HKCU_stringbuilder.Append(compare_HKCU_registry_changes_command);
             run_PowerShell_Command(Compare_HKCU_stringbuilder.ToString());
@@ -54,8 +54,8 @@ namespace Registry_Change_Display
 
 
             //create the file for the current state of HKLM and push data into it
-            string HKLM_Current_FilePath = string.Format($@"{0}/Current-HKLM-{1}.txt", path, DateTime.Now.ToShortDateString());
-            File.CreateText(HKLM_Current_FilePath);
+            string HKLM_Current_FilePath = string.Format($@"{0}\\Current-HKLM-{1}.txt", path, DateTime.Now.ToShortDateString());
+            File.Create(HKLM_Current_FilePath);
                 StringBuilder Current_HKLM_stringbuilder = new StringBuilder();
             string current_registry_HKLM_command = string.Format(
                     $@"dir -rec -erroraction ignore HKLM:\ | % name > {0}", HKLM_Current_FilePath);
@@ -65,7 +65,7 @@ namespace Registry_Change_Display
 
                 StringBuilder Compare_HKLM_stringbuilder = new StringBuilder();
             string compare_HKLM_registry_changes_command = string.Format(
-                    $@"Compare-Object (Get-Content -Path .\Base-HKLM.txt)(Get-Content-Path {0})", HKLM_Current_FilePath);
+                    $@"Compare-Object (Get-Content -Path .\\Base-HKLM.txt)(Get-Content-Path {0})", HKLM_Current_FilePath);
                     Compare_HKLM_stringbuilder.Clear();
                     Compare_HKLM_stringbuilder.Append(compare_HKLM_registry_changes_command);
                 run_PowerShell_Command(Compare_HKLM_stringbuilder.ToString());
